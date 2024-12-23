6ac3329fd5cf6e8cdc4dfc2d50c518cd5cbd76d7
Cerr1738 <ezechisom966@gmail.com>
2024-12-23 15:33:03 +0100

    final commit

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -11,7 +11,7 @@ const NavBar2 = () => {
       <nav className="p-4 bg-[#503025]  flex items-center justify-between font-small">
             <div>
             <Link to="/">
-                <img src={Logo} alt="Logo" className=" mx-[100px]" />
+                <img src={Logo} alt="Logo" className=" mx-[100px] mr-[140px]" />
             </Link>
             </div>
 
@@ -59,7 +59,7 @@ const NavBar2 = () => {
             </ul>
             </div>
             <div className="flex items-center gap-4 mx-[120px]">
-                <button className="p-2 rounded-full hover:bg-gray-100">
+                <button className="p-2 rounded-full hover:bg-gray-100 text-white">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
                   </svg>
